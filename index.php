751b024020f347019f5a71e22b1ed6468194206c
Janjos <joaoanjos_96@hotmail.com>
2017-12-04 13:10:35 +0000

    columnizer

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 <body>
 
 <nav>
+
     <ul>
         <h1 id="title">GAEA</h1>
         <li class="mainnav">About</li>
@@ -25,14 +26,15 @@
         <li class="mainnav">Articles</li>
         <li class="mainnav">Contacts</li>
         <li class="mainnav">Shop</li>
-        <li class="dropdown mainnav">Social</li>
-        <div class="dropdown-content">
-            <ul>
-                <li>Facebook</li>
-                <li>Linked in</li>
-                <li>Vimeo</li>
-            </ul>
-        </div>
+        <li class="dropdown mainnav">Social
+            <div class="dropdown-content">
+                <ul>
+                    <li>Facebook</li>
+                    <li>Linked in</li>
+                    <li>Vimeo</li>
+                </ul>
+            </div>
+        </li>
     </ul>
 
 </nav>
