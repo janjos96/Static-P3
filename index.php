13da16a872e0ad4d31ef25e4324d2501a9c5ffef
Janjos <joaoanjos_96@hotmail.com>
2017-12-04 12:37:41 +0000

    Boilerplate

--- index.php
+++ index.php
@@ -1,103 +1,196 @@
-<!DOCTYPE html>
-<html>
-
+<!doctype html>
+<html class="no-js" lang="pt">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <script src="jquery-3.2.1.min.js"></script>
-</head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
 <body>
 
-    <nav>
-        <ul>
-            <h1 id="title">GAEA</h1>
-            <li class="mainnav">About</li>
-            <li class="mainnav">Editions</li>
-            <li class="mainnav">Articles</li>
-            <li class="mainnav">Contacts</li>
-            <li class="mainnav">Shop</li>
-            <li class="dropdown mainnav">
-                <p>Social</p>
-                <div class="dropdown-content">
-                    <ul>
-                        <li>Facebook</li>
-                        <li>Linked in</li>
-                        <li>Vimeo</li>
+<nav>
+    <ul>
+        <h1 id="title">GAEA</h1>
+        <li class="mainnav">About</li>
+        <li class="mainnav">Editions</li>
+        <li class="mainnav">Articles</li>
+        <li class="mainnav">Contacts</li>
+        <li class="mainnav">Shop</li>
+        <li class="dropdown mainnav">Social</li>
+        <div class="dropdown-content">
+            <ul>
+                <li>Facebook</li>
+                <li>Linked in</li>
+                <li>Vimeo</li>
+            </ul>
+        </div>
+    </ul>
 
-                    </ul>
-                </div>
+</nav>
 
-            </li>
+<div id="language"><a>PT</a>/<a>ENG</a></div>
 
-        </ul>
+<div id="im"><img id="imagemain" src="img/main.png" alt="main"></div>
 
-    </nav>
 
-    <div id="language"><a>PT</a>/<a>ENG</a></div>
+<div class="contentarea">
 
-   <div id="im"><img id="imagemain" src="main.png" alt="main"></div> 
-    
-    
-    <div class="contentarea">
-        
-        
-         <div id="content">
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-   </div>
-        
-        <style type="text/css">
-body {width:100%; min-height:100%;}
 
-#content {width:100%; max-width:800px; margin:0 auto;}
+    <div id="content">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
 
-.box { float: left; display: table; height: 180px; 
-      width: 180px; margin:10px; background-color:blue;}
-</style>
-    
+    <style type="text/css">
+        body {
+            width: 100%;
+            min-height: 100%;
+        }
+
+        #content {
+            width: 100%;
+            max-width: 800px;
+            margin: 0 auto;
+        }
+
+        .box {
+            float: left;
+            display: table;
+            height: 180px;
+            width: 180px;
+            margin: 10px;
+            background-color: blue;
+        }
+    </style>
+
     <section id="latestarticles">
-    <h1>LATEST ARTICLES</h1>
-        
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla pretium nisl, sed euismod lorem. Quisque a tortor non velit pharetra aliquet. Nunc gravida metus ultricies porttitor pellentesque. Morbi mattis ut justo sed rhoncus. Suspendisse fringilla ante ut lobortis laoreet. Nulla at nisl vitae purus posuere consectetur vel a neque. Aliquam id ex erat. In gravida iaculis eros, sed tempus justo fermentum quis. Suspendisse aliquam justo magna, nec congue felis rhoncus quis. Sed ut nibh et elit aliquet aliquet. Proin sapien lectus, tempus nec ullamcorper vel, bibendum ac sem. Praesent malesuada, dui eget feugiat fermentum, dui enim euismod nisi, non pretium urna velit eu erat. Vivamus sed libero vel diam semper elementum non ac sapien. Donec ut orci malesuada, semper dolor in, tempus arcu.
+        <h1>LATEST ARTICLES</h1>
 
-Cras at erat nunc. Nunc aliquam nulla et libero gravida pharetra. Donec id tristique nisi. Quisque ut ligula quis dolor venenatis dignissim. Vestibulum fringilla quam id nisi lobortis sodales. Phasellus in risus et nibh posuere dictum at in orci. Fusce lacus neque, egestas eget quam id, sagittis ultricies urna. Sed laoreet dui a justo volutpat, tempor consectetur erat dictum. Suspendisse ultricies lorem efficitur, tincidunt odio id, consequat eros. Fusce maximus eleifend est, sollicitudin dictum purus consequat vitae. Suspendisse sit amet ipsum vel ipsum pretium volutpat. Proin faucibus, justo vel pretium tincidunt, leo nunc iaculis lacus, vitae consequat tellus leo at diam. Ut dapibus ligula magna, id pulvinar arcu sodales quis. Nulla vel pretium ante. Sed tincidunt, orci a placerat auctor, erat ex auctor lacus, in convallis nisl leo vitae libero.
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla pretium nisl, sed euismod lorem.
+            Quisque a tortor non velit pharetra aliquet. Nunc gravida metus ultricies porttitor pellentesque. Morbi
+            mattis ut justo sed rhoncus. Suspendisse fringilla ante ut lobortis laoreet. Nulla at nisl vitae purus
+            posuere consectetur vel a neque. Aliquam id ex erat. In gravida iaculis eros, sed tempus justo fermentum
+            quis. Suspendisse aliquam justo magna, nec congue felis rhoncus quis. Sed ut nibh et elit aliquet aliquet.
+            Proin sapien lectus, tempus nec ullamcorper vel, bibendum ac sem. Praesent malesuada, dui eget feugiat
+            fermentum, dui enim euismod nisi, non pretium urna velit eu erat. Vivamus sed libero vel diam semper
+            elementum non ac sapien. Donec ut orci malesuada, semper dolor in, tempus arcu.
 
-Quisque sed enim erat. Phasellus ultrices nulla ac tortor placerat, in facilisis tortor hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed blandit in diam ac blandit. In hac habitasse platea dictumst. Sed venenatis nulla sit amet cursus elementum. Suspendisse ut lacus vitae nulla feugiat pellentesque vel pulvinar purus. Praesent tincidunt hendrerit leo cursus ultricies. Proin eu eros tellus. Praesent elementum rutrum ex, et lacinia est consectetur id. Aenean lobortis odio urna, vel dignissim enim vulputate vel.
+            Cras at erat nunc. Nunc aliquam nulla et libero gravida pharetra. Donec id tristique nisi. Quisque ut ligula
+            quis dolor venenatis dignissim. Vestibulum fringilla quam id nisi lobortis sodales. Phasellus in risus et
+            nibh posuere dictum at in orci. Fusce lacus neque, egestas eget quam id, sagittis ultricies urna. Sed
+            laoreet dui a justo volutpat, tempor consectetur erat dictum. Suspendisse ultricies lorem efficitur,
+            tincidunt odio id, consequat eros. Fusce maximus eleifend est, sollicitudin dictum purus consequat vitae.
+            Suspendisse sit amet ipsum vel ipsum pretium volutpat. Proin faucibus, justo vel pretium tincidunt, leo nunc
+            iaculis lacus, vitae consequat tellus leo at diam. Ut dapibus ligula magna, id pulvinar arcu sodales quis.
+            Nulla vel pretium ante. Sed tincidunt, orci a placerat auctor, erat ex auctor lacus, in convallis nisl leo
+            vitae libero.
 
-Etiam diam eros, efficitur quis ultrices a, ornare et metus. Suspendisse ligula mi, dapibus eu mauris a, ornare suscipit dui. Aliquam at aliquam justo. Morbi luctus vehicula risus et pellentesque. Suspendisse potenti. Proin fringilla, orci sed molestie consequat, lorem massa euismod tortor, vel mollis magna arcu ut libero. Aenean non finibus tellus.
+            Quisque sed enim erat. Phasellus ultrices nulla ac tortor placerat, in facilisis tortor hendrerit.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed blandit in
+            diam ac blandit. In hac habitasse platea dictumst. Sed venenatis nulla sit amet cursus elementum.
+            Suspendisse ut lacus vitae nulla feugiat pellentesque vel pulvinar purus. Praesent tincidunt hendrerit leo
+            cursus ultricies. Proin eu eros tellus. Praesent elementum rutrum ex, et lacinia est consectetur id. Aenean
+            lobortis odio urna, vel dignissim enim vulputate vel.
 
-Mauris placerat ornare augue, ac aliquet eros imperdiet a. In vestibulum molestie tortor sit amet malesuada. Aenean tincidunt ipsum eu sem pretium, quis maximus purus auctor. Maecenas bibendum diam a varius faucibus. Integer mollis in leo in congue. Ut porta erat congue mollis tempor. Donec nec nibh volutpat tortor facilisis tempor. Curabitur dictum elit dui, ac gravida magna sollicitudin sed. Sed bibendum posuere lectus at tristique. Cras in enim condimentum, tincidunt felis quis, tincidunt ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris at efficitur tellus. Mauris lobortis </p>
+            Etiam diam eros, efficitur quis ultrices a, ornare et metus. Suspendisse ligula mi, dapibus eu mauris a,
+            ornare suscipit dui. Aliquam at aliquam justo. Morbi luctus vehicula risus et pellentesque. Suspendisse
+            potenti. Proin fringilla, orci sed molestie consequat, lorem massa euismod tortor, vel mollis magna arcu ut
+            libero. Aenean non finibus tellus.
+
+            Mauris placerat ornare augue, ac aliquet eros imperdiet a. In vestibulum molestie tortor sit amet malesuada.
+            Aenean tincidunt ipsum eu sem pretium, quis maximus purus auctor. Maecenas bibendum diam a varius faucibus.
+            Integer mollis in leo in congue. Ut porta erat congue mollis tempor. Donec nec nibh volutpat tortor
+            facilisis tempor. Curabitur dictum elit dui, ac gravida magna sollicitudin sed. Sed bibendum posuere lectus
+            at tristique. Cras in enim condimentum, tincidunt felis quis, tincidunt ipsum. Interdum et malesuada fames
+            ac ante ipsum primis in faucibus. Mauris at efficitur tellus. Mauris lobortis </p>
     </section>
-    
+
     <section id="latesteditions">
-    <h1>LATEST EDITIONS</h1>
-        
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla pretium nisl, sed euismod lorem. Quisque a tortor non velit pharetra aliquet. Nunc gravida metus ultricies porttitor pellentesque. Morbi mattis ut justo sed rhoncus. Suspendisse fringilla ante ut lobortis laoreet. Nulla at nisl vitae purus posuere consectetur vel a neque. Aliquam id ex erat. In gravida iaculis eros, sed tempus justo fermentum quis. Suspendisse aliquam justo magna, nec congue felis rhoncus quis. Sed ut nibh et elit aliquet aliquet. Proin sapien lectus, tempus nec ullamcorper vel, bibendum ac sem. Praesent malesuada, dui eget feugiat fermentum, dui enim euismod nisi, non pretium urna velit eu erat. Vivamus sed libero vel diam semper elementum non ac sapien. Donec ut orci malesuada, semper dolor in, tempus arcu.
+        <h1>LATEST EDITIONS</h1>
 
-Cras at erat nunc. Nunc aliquam nulla et libero gravida pharetra. Donec id tristique nisi. Quisque ut ligula quis dolor venenatis dignissim. Vestibulum fringilla quam id nisi lobortis sodales. Phasellus in risus et nibh posuere dictum at in orci. Fusce lacus neque, egestas eget quam id, sagittis ultricies urna. Sed laoreet dui a justo volutpat, tempor consectetur erat dictum. Suspendisse ultricies lorem efficitur, tincidunt odio id, consequat eros. Fusce maximus eleifend est, sollicitudin dictum purus consequat vitae. Suspendisse sit amet ipsum vel ipsum pretium volutpat. Proin faucibus, justo vel pretium tincidunt, leo nunc iaculis lacus, vitae consequat tellus leo at diam. Ut dapibus ligula magna, id pulvinar arcu sodales quis. Nulla vel pretium ante. Sed tincidunt, orci a placerat auctor, erat ex auctor lacus, in convallis nisl leo vitae libero.
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla pretium nisl, sed euismod lorem.
+            Quisque a tortor non velit pharetra aliquet. Nunc gravida metus ultricies porttitor pellentesque. Morbi
+            mattis ut justo sed rhoncus. Suspendisse fringilla ante ut lobortis laoreet. Nulla at nisl vitae purus
+            posuere consectetur vel a neque. Aliquam id ex erat. In gravida iaculis eros, sed tempus justo fermentum
+            quis. Suspendisse aliquam justo magna, nec congue felis rhoncus quis. Sed ut nibh et elit aliquet aliquet.
+            Proin sapien lectus, tempus nec ullamcorper vel, bibendum ac sem. Praesent malesuada, dui eget feugiat
+            fermentum, dui enim euismod nisi, non pretium urna velit eu erat. Vivamus sed libero vel diam semper
+            elementum non ac sapien. Donec ut orci malesuada, semper dolor in, tempus arcu.
 
-Quisque sed enim erat. Phasellus ultrices nulla ac tortor placerat, in facilisis tortor hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed blandit in diam ac blandit. In hac habitasse platea dictumst. Sed venenatis nulla sit amet cursus elementum. Suspendisse ut lacus vitae nulla feugiat pellentesque vel pulvinar purus. Praesent tincidunt hendrerit leo cursus ultricies. Proin eu eros tellus. Praesent elementum rutrum ex, et lacinia est consectetur id. Aenean lobortis odio urna, vel dignissim enim vulputate vel.
+            Cras at erat nunc. Nunc aliquam nulla et libero gravida pharetra. Donec id tristique nisi. Quisque ut ligula
+            quis dolor venenatis dignissim. Vestibulum fringilla quam id nisi lobortis sodales. Phasellus in risus et
+            nibh posuere dictum at in orci. Fusce lacus neque, egestas eget quam id, sagittis ultricies urna. Sed
+            laoreet dui a justo volutpat, tempor consectetur erat dictum. Suspendisse ultricies lorem efficitur,
+            tincidunt odio id, consequat eros. Fusce maximus eleifend est, sollicitudin dictum purus consequat vitae.
+            Suspendisse sit amet ipsum vel ipsum pretium volutpat. Proin faucibus, justo vel pretium tincidunt, leo nunc
+            iaculis lacus, vitae consequat tellus leo at diam. Ut dapibus ligula magna, id pulvinar arcu sodales quis.
+            Nulla vel pretium ante. Sed tincidunt, orci a placerat auctor, erat ex auctor lacus, in convallis nisl leo
+            vitae libero.
 
-Etiam diam eros, efficitur quis ultrices a, ornare et metus. Suspendisse ligula mi, dapibus eu mauris a, ornare suscipit dui. Aliquam at aliquam justo. Morbi luctus vehicula risus et pellentesque. Suspendisse potenti. Proin fringilla, orci sed molestie consequat, lorem massa euismod tortor, vel mollis magna arcu ut libero. Aenean non finibus tellus.
+            Quisque sed enim erat. Phasellus ultrices nulla ac tortor placerat, in facilisis tortor hendrerit.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed blandit in
+            diam ac blandit. In hac habitasse platea dictumst. Sed venenatis nulla sit amet cursus elementum.
+            Suspendisse ut lacus vitae nulla feugiat pellentesque vel pulvinar purus. Praesent tincidunt hendrerit leo
+            cursus ultricies. Proin eu eros tellus. Praesent elementum rutrum ex, et lacinia est consectetur id. Aenean
+            lobortis odio urna, vel dignissim enim vulputate vel.
 
-Mauris placerat ornare augue, ac aliquet eros imperdiet a. In vestibulum molestie tortor sit amet malesuada. Aenean tincidunt ipsum eu sem pretium, quis maximus purus auctor. Maecenas bibendum diam a varius faucibus. Integer mollis in leo in congue. Ut porta erat congue mollis tempor. Donec nec nibh volutpat tortor facilisis tempor. Curabitur dictum elit dui, ac gravida magna sollicitudin sed. Sed bibendum posuere lectus at tristique. Cras in enim condimentum, tincidunt felis quis, tincidunt ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris at efficitur tellus. Mauris lobortis </p>
+            Etiam diam eros, efficitur quis ultrices a, ornare et metus. Suspendisse ligula mi, dapibus eu mauris a,
+            ornare suscipit dui. Aliquam at aliquam justo. Morbi luctus vehicula risus et pellentesque. Suspendisse
+            potenti. Proin fringilla, orci sed molestie consequat, lorem massa euismod tortor, vel mollis magna arcu ut
+            libero. Aenean non finibus tellus.
+
+            Mauris placerat ornare augue, ac aliquet eros imperdiet a. In vestibulum molestie tortor sit amet malesuada.
+            Aenean tincidunt ipsum eu sem pretium, quis maximus purus auctor. Maecenas bibendum diam a varius faucibus.
+            Integer mollis in leo in congue. Ut porta erat congue mollis tempor. Donec nec nibh volutpat tortor
+            facilisis tempor. Curabitur dictum elit dui, ac gravida magna sollicitudin sed. Sed bibendum posuere lectus
+            at tristique. Cras in enim condimentum, tincidunt felis quis, tincidunt ipsum. Interdum et malesuada fames
+            ac ante ipsum primis in faucibus. Mauris at efficitur tellus. Mauris lobortis </p>
     </section>
     <footer>
         <p>GAEA® is a registered trademark.</p>
-      
+
     </footer>
 </div>
 
 
-</body>
+<!--[if lte IE 9]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade
+    your browser</a> to improve your experience and security.</p>
+<![endif]-->
 
+<!-- Add your site or application content here -->
+<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+    window.ga = function () {
+        ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('send', 'pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
 </html>
