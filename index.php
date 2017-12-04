82b88a1a98a139beef0061103c730df969aec41c
Ivo Amaro <ivo.amaro.97.p@gmail.com>
2017-12-04 17:32:02 +0000

    quadrados

--- index.php
+++ index.php
@@ -11,50 +11,58 @@
 
 <div id="im"><img id="imagemain" src="img/main.png" alt="main"></div>
 
-
 <div class="contentarea">
-    
+
+
+
 
     <section id="latestarticles">
         <h1>LATEST ARTICLES</h1>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla pretium nisl, sed euismod lorem.
-            Quisque a tortor non velit pharetra aliquet. Nunc gravida metus ultricies porttitor pellentesque. Morbi
-            mattis ut justo sed rhoncus. Suspendisse fringilla ante ut lobortis laoreet. Nulla at nisl vitae purus
-            posuere consectetur vel a neque. Aliquam id ex erat. In gravida iaculis eros, sed tempus justo fermentum
-            quis. Suspendisse aliquam justo magna, nec congue felis rhoncus quis. Sed ut nibh et elit aliquet aliquet.
-            Proin sapien lectus, tempus nec ullamcorper vel, bibendum ac sem. Praesent malesuada, dui eget feugiat
-            fermentum, dui enim euismod nisi, non pretium urna velit eu erat. Vivamus sed libero vel diam semper
-            elementum non ac sapien. Donec ut orci malesuada, semper dolor in, tempus arcu.
+        <div class="grid">
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2 grid-item--height2"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+        </div>
 
-            Cras at erat nunc. Nunc aliquam nulla et libero gravida pharetra. Donec id tristique nisi. Quisque ut ligula
-            quis dolor venenatis dignissim. Vestibulum fringilla quam id nisi lobortis sodales. Phasellus in risus et
-            nibh posuere dictum at in orci. Fusce lacus neque, egestas eget quam id, sagittis ultricies urna. Sed
-            laoreet dui a justo volutpat, tempor consectetur erat dictum. Suspendisse ultricies lorem efficitur,
-            tincidunt odio id, consequat eros. Fusce maximus eleifend est, sollicitudin dictum purus consequat vitae.
-            Suspendisse sit amet ipsum vel ipsum pretium volutpat. Proin faucibus, justo vel pretium tincidunt, leo nunc
-            iaculis lacus, vitae consequat tellus leo at diam. Ut dapibus ligula magna, id pulvinar arcu sodales quis.
-            Nulla vel pretium ante. Sed tincidunt, orci a placerat auctor, erat ex auctor lacus, in convallis nisl leo
-            vitae libero.
+        <script>
+            //isotype js
 
-            Quisque sed enim erat. Phasellus ultrices nulla ac tortor placerat, in facilisis tortor hendrerit.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed blandit in
-            diam ac blandit. In hac habitasse platea dictumst. Sed venenatis nulla sit amet cursus elementum.
-            Suspendisse ut lacus vitae nulla feugiat pellentesque vel pulvinar purus. Praesent tincidunt hendrerit leo
-            cursus ultricies. Proin eu eros tellus. Praesent elementum rutrum ex, et lacinia est consectetur id. Aenean
-            lobortis odio urna, vel dignissim enim vulputate vel.
+            var count = 0;
 
-            Etiam diam eros, efficitur quis ultrices a, ornare et metus. Suspendisse ligula mi, dapibus eu mauris a,
-            ornare suscipit dui. Aliquam at aliquam justo. Morbi luctus vehicula risus et pellentesque. Suspendisse
-            potenti. Proin fringilla, orci sed molestie consequat, lorem massa euismod tortor, vel mollis magna arcu ut
-            libero. Aenean non finibus tellus.
+            var $grid = $('.grid').isotope({
+                itemSelector: '.grid-item',
+                masonry: {
+                    columnWidth: 100
+                }
+            });
 
-            Mauris placerat ornare augue, ac aliquet eros imperdiet a. In vestibulum molestie tortor sit amet malesuada.
-            Aenean tincidunt ipsum eu sem pretium, quis maximus purus auctor. Maecenas bibendum diam a varius faucibus.
-            Integer mollis in leo in congue. Ut porta erat congue mollis tempor. Donec nec nibh volutpat tortor
-            facilisis tempor. Curabitur dictum elit dui, ac gravida magna sollicitudin sed. Sed bibendum posuere lectus
-            at tristique. Cras in enim condimentum, tincidunt felis quis, tincidunt ipsum. Interdum et malesuada fames
-            ac ante ipsum primis in faucibus. Mauris at efficitur tellus. Mauris lobortis </p>
+            $grid.on( 'click', '.grid-item', function() {
+                count++;
+                // change size of item by toggling gigante class
+                $( this ).toggleClass('gigante');
+                $grid.isotope('layout');
+
+            });
+
+        </script>
     </section>
 
     <section id="latesteditions">
@@ -128,6 +136,9 @@
     ga('create', 'UA-XXXXX-Y', 'auto');
     ga('send', 'pageview')
 </script>
+
+
+
 <script src="https://www.google-analytics.com/analytics.js" async defer></script>
 </body>
 </html>
