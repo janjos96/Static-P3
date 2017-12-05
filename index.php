5749cafed8809177ea03f123add3596ef3690743
Janjos <joaoanjos_96@hotmail.com>
2017-12-05 23:47:55 +0000

    Revert "Revert "Merge branch 'master' of https://github.com/janjos96/Static-P3""
    This reverts commit cb68b94ddd5e0d51dc37252d1258d95d435d0438.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <body>
 
 
+<<<<<<< HEAD
 <div class="row">
     <div class="col-2">
         <?php include 'parts/navbar.php' ?>
@@ -167,6 +168,81 @@
 
         </footer>
     </div>
+=======
+<div id="language"><a>PT</a>/<a>ENG</a></div>
+
+<div id="im"><img id="imagemain" src="img/main.png" alt="main"></div>
+<div class="vl"></div>
+
+
+<div class="contentarea">
+
+
+    <section id="latestarticles">
+
+            <h1>LATEST ARTICLES</h1>
+
+            <div class="grid">
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item"><a class="viewmore">view more</a></div>
+                <div class="grid-item"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2 grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item"><a class="viewmore">view more</a></div>
+                <div class="grid-item"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item"><a class="viewmore">view more</a></div>
+                <div class="grid-item"><a class="viewmore">view more</a></div>
+            </div>
+
+
+
+
+    </section>
+
+    <section id="latesteditions">
+        <h1>EDITIONS</h1>
+
+        <div class="grid">
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2 grid-item--height2"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+        </div>
+
+
+    </section>
+    <footer>
+        <p>GAEA® is a registered trademark.</p>
+
+    </footer>
+>>>>>>> 6494a958332d753fd2b2ab8b8b5a5a50a136b040
 </div>
 
 
@@ -181,7 +257,7 @@
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
 <script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
 
 <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 <script>
@@ -193,6 +269,8 @@
     ga('create', 'UA-XXXXX-Y', 'auto');
     ga('send', 'pageview')
 </script>
+
+
 <script src="https://www.google-analytics.com/analytics.js" async defer></script>
 </body>
 </html>
