cb68b94ddd5e0d51dc37252d1258d95d435d0438
Janjos <joaoanjos_96@hotmail.com>
2017-12-05 23:47:44 +0000

    Revert "Merge branch 'master' of https://github.com/janjos96/Static-P3"
    This reverts commit 5b53369c09d391dcfb0370010b2da68ef04db433, reversing
    changes made to fa4e13752b1679c45f5c40db7bebe6beaf355967.

--- parts/head.php
+++ parts/head.php
@@ -2,7 +2,7 @@
 print '<head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>GAEA</title>
+    <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -12,9 +12,7 @@ print '<head>
 
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/isotype.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    
     
     <link href="plugs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="plugs/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -22,7 +20,6 @@ print '<head>
     <script src="plugs/jquery/jquery.min.js"></script>
     <script src="plugs/popper/popper.min.js"></script>
     <script src="plugs/bootstrap/js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
 </head>';
 
 ?>
