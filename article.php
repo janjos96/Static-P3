7e1d48e896140d2bed50ecea64e26c47f76b9857
Janjos <joaoanjos_96@hotmail.com>
2017-12-21 14:46:32 +0000

    article 1 & 2

--- article.php
+++ article.php
@@ -53,17 +53,16 @@
     <div class="col-2">
         <?php include 'parts/navbar.php' ?>
     </div>
-    <div class="col-10">
-        <div id="language"><a>PT</a>/<a>ENG</a></div>
+    <div class="col-9">
 
         <section id="latestarticles">
 
             <h1>Apocalypse juggernaut, hello!</h1>
 
-            <img src="img/astronauta2.jpg" style="width: 100%;">
+            <img src="img/astronauta2.jpg" style="width: 100%; margin-bottom: 2rem;">
 
             <div class="wrapper">
-                <div class="wide1" style="width: 50%">
+                <div class="wide1">
                     <p>As if the spirits of our ancestors weren't trouble enough, now we're haunted by the ghosts of our
                         descendants. Ken Kesey claims that ecology is the current handy smokescreen for everybody's Dire
                         Report — the voice of white-eyed paranoia. Gary Snyder (strong proponent of population control)
@@ -75,7 +74,8 @@
                         surprises. Which is what we're here for. The Standard Operating Out, when a species is in a
                         bind, is to diversify. Multiply alternatives. If you don't know what's coming, the way to evolve
                         ahead of the changes is to try everything.</p>
-                    <p>Reasonable laws made by reasonable men in reasonable times proscribe trying everything. For a
+                    <p class="dontsplit">Reasonable laws made by reasonable men in reasonable times proscribe trying
+                        everything. For a
                         good reason: people get hurt trying stuff. If you're bound to try stuff anyway, then either
                         you're working directly for City Hall or you're an outlaw, or both.</p>
                     <p>Buckminster Fuller: "It surprises people when you tell them that since the last ice age
@@ -92,96 +92,129 @@
             </div>
 
             <div class="wrapper">
-                <div class="wide2" style="width: 100%">
+                <div class="wide2">
 
-                    <img src="img/head_universe3.jpg" style="width: 100%;">
-
-                    <p>Present outlaw areas include space, the oceans, mountains, desert, northwoods and tundra, jungle,
-                        and then places like molecular biology, your mind, and other state-of-theart frontiers whose
-                        languages are still foreign to lawmakers. One thing we need is better outlaws. The antidote to
-                        external routine is internal routine, not sloppiness. The finding and following of your own
-                        drum. Help save the world: do something wrong today. Do it today because time is short, and
-                        capable outlaws aren't made overnight Is it a contradiction to design an outlaw area?
-                        In the past there was always undesignable frontier to serve, some space left over or not reached
-                        yet. If we run out of that resource, I can we synthesize it, or intentionally preserve it? Or
-                        will it spontaneously bubble back up in the middle of our cities? (Oh yes.) I suggest that we
-                        must give the devil his due or I he will surely extract it. Any design fantasy is just loose
-                        talk until it happens. Here's I one. — SB
-                    </p>
-                    <p>Loose Talk 1: The Outlaw Area
-                        The specific fantasy called OUTLAW AREA is a geographical place where anything goes. Let's say
-                        it's an island. For society it functions as a human dumping station. When a guy gets a jail
-                        sentence he has the choice of serving the sentence or being deported to the Island. He can't
-                        come back for some specific period of time, or he must meet some bureaucratic criteria to
-                        return, or the expulsion is permanent. Yessir, put all the bad apples in one bad apple barrel.
-                        For the individual it serves as an always possible alternative to the situation he's in. rie can
-                        simply split for the Island and take his chances there. No one's going to try to get him back.
-                        For evolution it's an open end, a place for other stuff to happen. High energy, minimum prior
-                        form, violent range of constant inputs; it should be wild. "In wildness is the preservation of
-                        the world."
-                        It's difficult to imagine how it would be in the Outlaw Area. You can project some from the
-                        history of Australia, once a penal colony, or from the present underworld. Robert Heinlein has a
-                        book about the Moon as a penal colony called “The Moon is a Harsh Mistress”. It's kind of nice:
-                        they have a revolution and throw the jailers out. Depending on the size of the island, my
-                        expectation would be either a dictatorship or warfare of rival gangs, like in old China,
-                        warlords. Maybe not though. This is a different age. The Island might become an Aquarian haven
-                        for the most useful pioneers — the cream and the dregs of society, (Kesey's notion) bending
-                        reality off into unimaginable directions with no restrictions save the harsh ones of nature.
-                    </p>
+                    <div class="row">
+                        <div class="col-6">
+                            <p>Present outlaw areas include space, the oceans, mountains, desert, northwoods and tundra,
+                                jungle,
+                                and then places like molecular biology, your mind, and other state-of-theart frontiers
+                                whose
+                                languages are still foreign to lawmakers. One thing we need is better outlaws. The
+                                antidote
+                                to
+                                external routine is internal routine, not sloppiness. The finding and following of your
+                                own
+                                drum. Help save the world: do something wrong today. Do it today because time is short,
+                                and
+                                capable outlaws aren't made overnight Is it a contradiction to design an outlaw area?
+                                In the past there was always undesignable frontier to serve, some space left over or not
+                                reached
+                                yet. If we run out of that resource, I can we synthesize it, or intentionally preserve
+                                it?
+                                Or
+                                will it spontaneously bubble back up in the middle of our cities? (Oh yes.) I suggest
+                                that
+                                we
+                                must give the devil his due or I he will surely extract it. Any design fantasy is just
+                                loose
+                                talk until it happens. Here's I one. — SB
+                            </p>
+                        </div>
+                        <div class="col-6">
+                            <img class="columnbreak" src="img/head_universe3.jpg" style="width: 100%; margin-bottom: 2rem; margin-top: 1rem;">
+                        </div>
+                    </div>
                 </div>
-            </div>
+
+                <div class="wrapper">
+                    <div class="wide3">
+                        <p>Loose Talk 1: The Outlaw Area
+                            The specific fantasy called OUTLAW AREA is a geographical place where anything goes. Let's
+                            say
+                            it's an island. For society it functions as a human dumping station. When a guy gets a jail
+                            sentence he has the choice of serving the sentence or being deported to the Island. He can't
+                            come back for some specific period of time, or he must meet some bureaucratic criteria to
+                            return, or the expulsion is permanent. Yessir, put all the bad apples in one bad apple
+                            barrel.
+                            For the individual it serves as an always possible alternative to the situation he's in. rie
+                            can
+                            simply split for the Island and take his chances there. No one's going to try to get him
+                            back.
+                            For evolution it's an open end, a place for other stuff to happen. High energy, minimum
+                            prior
+                            form, violent range of constant inputs; it should be wild. "In wildness is the preservation
+                            of
+                            the world."
+                            It's difficult to imagine how it would be in the Outlaw Area. You can project some from the
+                            history of Australia, once a penal colony, or from the present underworld. Robert Heinlein
+                            has a
+                            book about the Moon as a penal colony called “The Moon is a Harsh Mistress”. It's kind of
+                            nice:
+                            they have a revolution and throw the jailers out. Depending on the size of the island, my
+                            expectation would be either a dictatorship or warfare of rival gangs, like in old China,
+                            warlords. Maybe not though. This is a different age. The Island might become an Aquarian
+                            haven
+                            for the most useful pioneers — the cream and the dregs of society, (Kesey's notion) bending
+                            reality off into unimaginable directions with no restrictions save the harsh ones of nature.
+                        </p>
+                    </div>
+                </div>
 
 
         </section>
 
-        <footer>
-            <p>GAEA® is a registered trademark.</p>
+    </div>
+    <div class="col-1"><a>PT</a>/<a>ENG</a></div>
+</div>
 
-        </footer>
-
-
-        <!--[if lte IE 9]>
-        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a
-                href="https://browsehappy.com/">upgrade
-            your browser</a> to improve your experience and security.</p>
-        <![endif]-->
-
-        <!-- Add your site or application content here -->
-        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-                integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/isotope.pkgd.min.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-        <script>
-            window.ga = function () {
-                ga.q.push(arguments)
-            };
-            ga.q = [];
-            ga.l = +new Date;
-            ga('create', 'UA-XXXXX-Y', 'auto');
-            ga('send', 'pageview')
-        </script>
+<footer>
+    <p>GAEA® is a registered trademark.</p>
+</footer>
 
 
-        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+<!--[if lte IE 9]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a
+        href="https://browsehappy.com/">upgrade
+    your browser</a> to improve your experience and security.</p>
+<![endif]-->
 
-        <script src="columnizer/src/jquery.columnizer.js" type="text/javascript" charset="utf-8"></script>
-        <script>
-            $(function () {
-                $('h1').addClass("dontend");
-                $('a').click(function () {
-                    alert("click!");
-                });
-                $('.wide1').columnize({
-                    columns: 1,
-                });
-                $('.wide2').columnize({
-                    columns: 3,
-                });
-            });
-        </script>
+<!-- Add your site or application content here -->
+<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+    window.ga = function () {
+        ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('send', 'pageview')
+</script>
+
+
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+
+<script src="columnizer/src/jquery.columnizer.js" type="text/javascript" charset="utf-8"></script>
+<script>
+    $(function () {
+        $('h1').addClass("dontend");
+        $('.wide1').columnize({
+            columns: 2,
+        });
+        $('.wide2').columnize({
+            columns: 1,
+        });
+        $('.wide3').columnize({
+            columns: 2,
+        });
+    });
+</script>
 </body>
 </html>
