e281ef47289fc417e012b66b9880999860702e0e
Ivo Amaro <ivo.amaro.97.p@gmail.com>
2017-12-27 19:02:06 +0000

    scroll nos artigos

--- articles.php
+++ articles.php
@@ -4,7 +4,8 @@
 <?php include 'parts/head.php' ?>
 
 <body>
-
+<div class="scrollbar" id="style-3">
+    <div class="force-overflow">
 <?php include 'parts/navbar.php' ?>
 
 <div id="language"><a>PT</a>/<a>ENG</a></div>
@@ -109,5 +110,8 @@
 
 
 <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+
+    </div>
+</div>
 </body>
 </html>
