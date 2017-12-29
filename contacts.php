85a7a29dcd5b9cb1fdccbadfaf23a66e171fc027
Ivo Amaro <ivo.amaro.97.p@gmail.com>
2017-12-29 14:50:22 +0000

    contacts

--- contacts.php
+++ contacts.php
@@ -6,33 +6,31 @@
 <body>
 <div class="scrollbar" id="style-3">
     <div class="force-overflow">
-<?php include 'parts/navbar.php' ?>
 
-<!--<div id="language"><a>PT</a>/<a>ENG</a></div>-->
-
-<div class="contentarea">
+        <div class="row">
+            <div class="col-2">
+                <?php include 'parts/navbar.php' ?>
+            </div>
+            <div class="col-10">
 
 
     <h1 id="tit">CONTACTS</h1>
 
     <div id="address">
-        ADDRESS
+        <p>Fell free to contact us anytime.</p>
 
-        Street
-        R Indústria Porta 104 <br>
-        City
-        Coja<br>
-        State Full
-        Coimbra<br>
-        Zip Code
-        3305-096
-        Phone Number<br>
-        21 239 329 1323
+        <br>
+        <i class="fa fa-map-marker" aria-hidden="true"></i>
+       <p> Street Indústria do Papel, Nº 104 <br>
+        3305-096 - Coimbra</p><br>
+        <i class="fa fa-phone" aria-hidden="true"></i>
+       <p> 21 239 329 1323</p>
 
 
 
     </div>
-
+<div class="emailform">
+    <i class="fa fa-envelope-o" aria-hidden="true"></i>
     <form action="envia.php" method="POST">
         <input type="text" name="name" placeholder="Name" required><br>
         <input type="text" name="email" placeholder="Email" required><br>
@@ -40,12 +38,13 @@
         <input class="inputButton" type="submit" value="Send">
         <input class="inputButton" type="reset" value="Reset">
     </form>
-
+</div>
 
     <footer>
         <p>GAEA® is a registered trademark.</p>
     </footer>
 </div>
+    </div>
 
 
 <!--[if lte IE 9]>
