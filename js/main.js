bf4a384346fb223ab6ccd4f28af0f25ded93eb45
Ivo Amaro <ivo.amaro.97.p@gmail.com>
2018-01-05 10:57:34 +0000

    css

--- js/main.js
+++ js/main.js
@@ -1,49 +1,50 @@
+$(document).ready(function () {
 
 
-$(document).ready(function(){
-
-
-
+    //  window.onload = function () {
 
 
 //-------------------------RESPONSIVE NAV-------------------------//
 
-    var count = 0;
+    if (window.innerWidth < 600) {
 
-    if (count === 0){
-        $("#responsive_nav ul").css("display", "none");
+        $("#navb ").css("display", "none");
 
-    } else {
-        $("#responsive_nav ul").css("display", "block");
-    }
+        var count = 0;
 
 
-    $("#btn").click(function(){
+        if (count === 0) {
+            $("#responsive_nav ul").css("display", "none");
 
-        if (count % 2 === 0) {
-            $("#responsive_nav ul").slideDown(0);
-            $("#responsive_nav").css("height", "100vh");
-            $(this).css("transform", " rotate(180deg)");
+        } else {
 
-            count+=1;
-        }else{
+            $("#responsive_nav ul").css("display", "block");
 
-
-            $("#responsive_nav ul").slideUp(0);
-            $("#responsive_nav ").css("height", "10vh");
-            $(this).css("transform", " rotate(360deg)");
-
-            count+=1;
         }
 
 
+        $("#btn").click(function () {
 
-    });
+
+            if (count % 2 === 0) {
+                $("#responsive_nav ul").slideDown(0);
+                $("#responsive_nav").css("height", "100vh");
+                $(this).css("transform", " rotate(180deg)");
+
+                count += 1;
+            } else {
 
 
-    if (window.innerWidth <= 600) {
+                $("#responsive_nav ul").slideUp(0);
+                $("#responsive_nav ").css("height", "10vh");
+                $(this).css("transform", " rotate(360deg)");
 
-        $("#navb ").css("display", "none");
+                count += 1;
+            }
+
+
+        });
+
 
     } else {
 
@@ -51,40 +52,36 @@ $(document).ready(function(){
 
     }
 
-    
+
     //------------------------------------------------//
 
-   if (location.pathname === '/Static-P3/index.php'){
+    if (location.pathname === '/Static-P3/index.php') {
 
-       $("#navb").css("display", "none");
-
-
-       $(".scrollbar").scroll(function(){
+        $("#navb").css("display", "none");
 
 
-
-           if($(this).scrollTop() >= 780){
-
-               /*  $("#navb").css("display", "block");*/
-               $("#navb").fadeIn(1000);
-
-           } else {
-               $("#navb").css("display", "none");
-
-           }
-       });
+        $(".scrollbar").scroll(function () {
 
 
+            if ($(this).scrollTop() >= 780) {
 
-   }else{
-       $("#navb").css("display", "block");
-   }
-
-
+                /*  $("#navb").fadeIn(0);*/
+                $("#navb").css("display", "block");
 
 
+            } else {
+                $("#navb").css("display", "none");
 
-    $(".scrollbar").scroll(function() {
+            }
+        });
+
+
+    } else {
+        $("#navb").css("display", "block");
+    }
+
+
+    $(".scrollbar").scroll(function () {
 
         var scroll_height = $(".force-overflow").height();
         var window_height = $(window).height();
@@ -93,7 +90,7 @@ $(document).ready(function(){
 
         var totalScroll = (scroll_top / (scroll_height - window_height) * 100);
 
-        $(".KW_progressBar").css("height", totalScroll + "vh" );
+        $(".KW_progressBar").css("height", totalScroll + "vh");
 
     });
 
@@ -103,25 +100,25 @@ $(document).ready(function(){
 //ISOTYPE JS
 
 
-var $grid = $('.grid').isotope({
-    itemSelector: '.grid-item',
-    transitionDuration: 0,
-    masonry: {
-        columnWidth: 100,
+    var $grid = $('.grid').isotope({
+        itemSelector: '.grid-item',
+        transitionDuration: 0,
+        masonry: {
+            columnWidth: 100,
 
-    }
-});
+        }
+    });
 
-$grid.on( 'click', '.grid-item', function() {
+    $grid.on('click', '.grid-item', function () {
 
-    // change size of item by toggling gigante class
-    $('.grid-item').removeClass('gigante');
-    $('.viewmore').css('display','none');
-    $(this).addClass('gigante');
-    $(this).find(".viewmore").fadeIn(0);
-    $grid.isotope('layout');
+        // change size of item by toggling gigante class
+        $('.grid-item').removeClass('gigante');
+        $('.viewmore').css('display', 'none');
+        $(this).addClass('gigante');
+        $(this).find(".viewmore").fadeIn(0);
+        $grid.isotope('layout');
 
-});
+    });
 
 
 //------------------------------------------------------------//
@@ -131,12 +128,16 @@ $grid.on( 'click', '.grid-item', function() {
 
     var scene = document.getElementById('scene');
 
-    var parallaxInstance = new Parallax(scene , {
+    var parallaxInstance = new Parallax(scene, {
         limitX: false,
         limitY: false,
 
     });
-  /*------------------------------------------*/
+    /*------------------------------------------*/
+
+    //   }
+
+
 });
 
 
