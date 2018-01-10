d047076884e2dfd158d9034b8ea1212cda31e07e
Ivo Amaro <ivo.amaro.97.p@gmail.com>
2018-01-10 16:54:36 +0000

    asdasd

--- index.php
+++ index.php
@@ -37,9 +37,7 @@
                 <li class="mainnav"><a href="articles.php">Articles</a></li>
                 <li class="mainnav"><a href="contacts.php">Contacts</li></a></li>
                 <li class="mainnav"><a href="shop.php">Shop</a></li>
-                <li class="mainnav social"><a><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a></li>
-                <li class="mainnav social"><a> <i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a></li>
-
+               
             </ul>
 
         </nav>
@@ -53,17 +51,23 @@
             <div data-depth="0.2"><img class="imagemain" src="img/paralaxlayers/6.png"></div>
             <div data-depth="0.3"><img class="imagemain" src="img/paralaxlayers/5.png"></div>
             <div data-depth="0.6"><img class="imagemain" src="img/paralaxlayers/4.png"></div>
-           <div data-depth="0"><img class="imagemain" id="logo" src="img/paralaxlayers/1.1.png"></div>
-            <div data-depth="0.7"><img class="imagemain" src="img/paralaxlayers/2.png"></div>
+            <div data-depth="0"><img class="imagemain" id="logo" src="img/paralaxlayers/1.1.png"></div>
+
             <div data-depth="0.8"><img class="imagemain" src="img/paralaxlayers/3.png"></div>
+            <div data-depth="0.7"><img class="imagemain" src="img/paralaxlayers/2.png"></div>
 
-
+            <div data-depth="0.2"><img class="imagemain" src="img/paralaxlayers/12.png"></div>
+            <div data-depth="0.8"><img class="imagemain" src="img/paralaxlayers/11.png"></div>
             <div data-depth="0.6"><img class="imagemain" src="img/paralaxlayers/10.png"></div>
             <div data-depth="0.4"><img class="imagemain" src="img/paralaxlayers/9.png"></div>
             <div data-depth="0.2"><img class="imagemain" src="img/paralaxlayers/8.png"></div>
 
-            <div data-depth="0.8"><img class="imagemain" src="img/paralaxlayers/11.png"></div>
-            <div data-depth="0.2"><img class="imagemain" src="img/paralaxlayers/12.png"></div>
+           <!-- <div data-depth="0.6"><img class="imagemain" src="img/paralaxlayers/14.png"></div>
+            <div data-depth="0.2"><img class="imagemain" src="img/paralaxlayers/13.png"></div>-->
+
+
+
+
 
 
 
@@ -106,17 +110,26 @@
 
                 <div class="grid-item grid-item--width2 grid-item--height2"><a class="viewmore" href="article3.php">Patterns in Nature</a></div>
                 <div class="grid-item grid-item--width2 grid-item--height2"><a class="viewmore">view more</a></div>
-                <div class="grid-item--width2 grid-item--height2"><a class="viewmore">view more</a></div>
-                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
-                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+
+                <div class="grid-item grid-item--height2"><a class="viewmore" href="article3.php">Patterns in Nature</a></div>
+                <div class="grid-item grid-item--width2 "><a class="viewmore">view more</a></div>
+
+                <div class="grid-item  grid-item--height2"><a class="viewmore" href="article3.php">Patterns in Nature</a></div>
                 <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
-                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
-                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+
+                <div class="grid-item grid-item--width2 grid-item--height2"><a class="viewmore" href="article3.php">Patterns in Nature</a></div>
                 <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+
+                <div class="grid-item grid-item--width2 grid-item--height2"><a class="viewmore" href="article3.php">Patterns in Nature</a></div>
+                <div class="grid-item  grid-item--height2"><a class="viewmore">view more</a></div>
+
+                <div class="grid-item grid-item--width2 grid-item--height2"><a class="viewmore" href="article3.php">Patterns in Nature</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
                 <div class="grid-item grid-item--width2 grid-item--height2"><a class="viewmore">view more</a></div>
-                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
-                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
-                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+
+
+
+
 
             </div>
 
